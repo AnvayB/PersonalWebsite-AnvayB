3f3df37ced384041b31fc4872e2743f9b19ad57a
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-11-18 20:01:48 -0800

    added down arrow on intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,6 @@
 import './Intro.scss';
 import Face from '../../images/face.png';
+import down from '../../images/down.png'
 
 //switch left and right classes later so that face is on right side
 
@@ -10,7 +11,12 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi, my name is</h2>
           <h1>Anvay Bhanap</h1>
+          <h3>Engineer<span></span></h3>
+          {/* software engineer, web developer */}
         </div>
+        <a href="#portfolio">
+          <img src={down} alt="" />
+        </a>
       </div>
 
       <div className="right">
