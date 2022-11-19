a16d18dad7062162ad1573db081b7fa4dd65b9a3
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-11-18 16:10:44 -0800

    hamburgesa menu made, potential edits commented in Menu.scss

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Portfolio from './components/portfolio/Portfolio'
 import Works from './components/works/Works'
 import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
+import Menu from './components/menu/Menu'
 import './App.scss'
 import { useState } from 'react'
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(true)
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
       <TopBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
 
         <Intro />
