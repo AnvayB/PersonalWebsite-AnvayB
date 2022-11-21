48bff14363b83a2b718f8c3ac11982c935d11211
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-11-20 19:57:53 -0800

    portfolio sections transition with separate component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,46 @@
 import './Portfolio.scss'
+import PortfolioList from '../portfolioList/PortfolioList';
+import { useState } from 'react';
 
 export default function Portfolio() {
+  
+  const [selected, setSelected] = useState("featured")
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured"
+    },
+    {
+      id: "web",
+      title: "Web App"
+    },
+    {
+      id: "mobile",
+      title: "Mobile App"
+    },
+    {
+      id: "design",
+      title: "Design"
+    },
+    {
+      id: "content",
+      title: "Content"
+    }
+
+  ];
+
   return (
     <div className='portfolio' id='portfolio'>
       <h1>Portfolio</h1>
       <ul>
-        <li className='active'>Featured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
-        <li>Design</li>
-        <li>Branding</li>
+        {list.map(item => (
+          <PortfolioList 
+            title={item.title} 
+            active={selected === item.id} setSelected={setSelected}
+            id={item.id}
+            />
+        ))}
       </ul>
       <div className="container">
 
@@ -22,7 +53,7 @@ export default function Portfolio() {
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt="" />
           <h3>Banking App</h3>
         </div>
-        
+
         <div className="item">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt="" />
           <h3>Banking App</h3>
