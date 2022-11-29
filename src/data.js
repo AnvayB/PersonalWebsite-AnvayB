504ad26b6de72c262e4e5798f7a910019301a9e4
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-11-29 02:29:18 -0800

    featured personalized, portfolio halfway

--- src/data.js
+++ src/data.js
@@ -1,138 +1,120 @@
+import html from './components/portfolio/portfolioImages/htmlcssjs.png';
+import cf from './components/portfolio/portfolioImages/codefellows.jpeg';
+import adobe from './components/portfolio/portfolioImages/adobe.png';
+import python from './components/portfolio/portfolioImages/python.png'
+
 export const featuredPortfolio = [
   {
     id: 1,
-    title: "Social Media App",
+    title: "San Jose State University",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://www.sjsu.edu/cies/pics/tower_hall_02.jpg",
+    dates: "Aug 2018 - Dec 2022"
   },
   {
     id: 2,
-    title: "Rampa UI Design",
+    title: "Code Fellows",
     img:
-      "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+      cf,
+    dates: "Jun 2021 - Aug 2021"
   },
   {
     id: 3,
-    title: "E-commerce Web Design",
+    title: "Milpitas High School",
     img:
-      "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
-  },
-  {
-    id: 4,
-    title: "Relax Mobile App",
-    img:
-      "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
-  },
-  {
-    id: 5,
-    title: "Hero Web Design",
-    img:
-      "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
-  },
-  {
-    id: 6,
-    title: "Banking App",
-    img:
-      "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
+      "https://webstores.activenetwork.com/school-software/milpitas_high_school/userdata/milpitas_high_school/photo.gif",
+    dates: "Aug 2014 - Jun 2018"
   },
 ];
 
 export const webPortfolio = [
   {
     id: 1,
-    title: "Web Social Media App",
-    img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    title: "Prismatic Softwares",
+    img: "https://i.ytimg.com/vi/rrA2oCUA584/mqdefault.jpg",
+    dates: "Jun 2022 - Aug 2022"
   },
   {
     id: 2,
-    title: "Web Rampa UI Design",
-    img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
+    title: "RippleMatch",
+    img: "https://d3ojh0z2ifymtd.cloudfront.net/homepage/RM_og_tag.png",
+    dates: "Jan 2021 - Apr 2021"
   },
   {
     id: 3,
-    title: "Web E-commerce Design",
-    img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
-  },
-  {
-    id: 4,
-    title: "Web Relax App",
-    img: "https://cdn.dribbble.com/users/2264844/screenshots/15463998/media/d85e92a332c41094f68e1f716884f7ce.jpg?compress=1&resize=1200x900",
-  },
-  {
-    id: 5,
-    title: "Web Design",
-    img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
-  },
-  {
-    id: 6,
-    title: "Web Banking App",
-    img: "https://cdn.dribbble.com/users/6384483/screenshots/15468426/media/ce9479fa1f8dba3a4a49840d76e55e31.png?compress=1&resize=1200x900",
+    title: "Success Learning Center",
+    img: "http://www.cliveden.com/images/facebook/advindiamag600.png",
+    dates: "Jan 2018 - Jun 2018"
   },
 ];
 
-export const mobilePortfolio = [
-  {
-    id: 1,
-    title: "Mobile Social Media App",
-    img:
-      "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
-  },
-  {
-    id: 2,
-    title: "Mobile Rampa UI Design",
-    img:
-      "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
-  },
-  {
-    id: 3,
-    title: "Mobile E-commerce Design",
-    img:
-      "https://cdn.dribbble.com/users/1998175/screenshots/15459384/media/48ac2b43ebe81ba0866afea1383cc939.png?compress=1&resize=1200x900",
-  },
-  {
-    id: 4,
-    title: "Mobile Relax App",
-    img:
-      "https://cdn.dribbble.com/users/2083704/screenshots/15468619/media/cd958306c7a772449e1ac23bd65ce506.png?compress=1&resize=1200x900",
-  },
-  {
-    id: 5,
-    title: "Mobile Hero Design",
-    img:
-      "https://cdn.dribbble.com/users/26642/screenshots/15462545/media/1a202ef5c8338b6f78634e6edc1ba350.png?compress=1&resize=1200x900",
-  },
-  {
-    id: 6,
-    title: "Mobile Banking App",
-    img:
-      "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
-  },
-];
+// export const mobilePortfolio = [
+//   {
+//     id: 1,
+//     title: "Mobile Social Media App",
+//     img:
+//       "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
+//   },
+//   {
+//     id: 2,
+//     title: "Mobile Rampa UI Design",
+//     img:
+//       "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
+//   },
+//   {
+//     id: 3,
+//     title: "Mobile E-commerce Design",
+//     img:
+//       "https://cdn.dribbble.com/users/1998175/screenshots/15459384/media/48ac2b43ebe81ba0866afea1383cc939.png?compress=1&resize=1200x900",
+//   },
+//   {
+//     id: 4,
+//     title: "Mobile Relax App",
+//     img:
+//       "https://cdn.dribbble.com/users/2083704/screenshots/15468619/media/cd958306c7a772449e1ac23bd65ce506.png?compress=1&resize=1200x900",
+//   },
+//   {
+//     id: 5,
+//     title: "Mobile Hero Design",
+//     img:
+//       "https://cdn.dribbble.com/users/26642/screenshots/15462545/media/1a202ef5c8338b6f78634e6edc1ba350.png?compress=1&resize=1200x900",
+//   },
+//   {
+//     id: 6,
+//     title: "Mobile Banking App",
+//     img:
+//       "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+//   },
+// ];
 
 export const designPortfolio = [
   {
     id: 1,
-    title: "Design Social Media App",
+    title: "ReactJS",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--qo_Wp38Z--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/e0nl7ziy1la7bpwj7rsp.png",
+      dates: "1.5 years"
   },
   {
     id: 2,
-    title: "Design Rampa UI Design",
+    title: "HTML/JS/CSS",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      html,
+    dates: "+1.5 years"
   },
   {
     id: 3,
-    title: "Design E-commerce Web Design",
+    title: "Python",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      python,
+    dates: "1 year"
   },
   {
     id: 4,
-    title: "Design Relax Mobile App",
+    title: "Photoshop, Lightroom, XD",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      adobe,
+    dates: "+2 years"
   },
   {
     id: 5,
